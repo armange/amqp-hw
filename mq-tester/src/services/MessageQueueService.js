bb857a35c122e5d7ff7e2b7d09617a2508669ae3
Diego Armange Costa <diego.armange.costa@gmail.com>
2020-04-04 20:53:22 -0300

    (mq-tester) Env variables

--- mq-tester/src/services/MessageQueueService.js
+++ mq-tester/src/services/MessageQueueService.js
@@ -4,28 +4,31 @@ export default {
     name: 'MessageQueueService',
     getMessage(queue, success, fail) {
         axios
-            .get('http://localhost:4321/amqp/'+queue+'/receive', 
-            {
-                auth: { 
-                   username: 'user', 
-                   password: '123456' 
-                }
-             })
+            .get(this.getBasePath() + queue + '/receive',
+                {
+                    auth: {
+                        username: 'user',
+                        password: '123456'
+                    }
+                })
             .then(success)
             .catch(fail)
     },
     sendMessage(queue, message, success, fail) {
         axios
-            .post('http://localhost:4321/amqp/'+queue+'/send', 
-            message 
-            , {
-                headers: { 'Content-Type': 'text/plain' },
-                auth: { 
-                   username: 'user', 
-                   password: '123456' 
-                }
-             })
+            .post(this.getBasePath() + queue + '/send',
+                message
+                , {
+                    headers: { 'Content-Type': 'text/plain' },
+                    auth: {
+                        username: 'user',
+                        password: '123456'
+                    }
+                })
             .then(success)
             .catch(fail)
+    },
+    getBasePath() {
+        return 'http://' + process.env.VUE_APP_MESSAGE_QUEUE_ENDPOINT_HOST + ':' + process.env.VUE_APP_MESSAGE_QUEUE_ENDPOINT_PORT + '/'
     }
 }
