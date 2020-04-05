258a7cc6d6822ed37ce95e2c7bf5cd03d1c3da53
Diego Armange Costa <diego.armange.costa@gmail.com>
2020-04-05 01:42:02 -0300

    (Service-one)disabling CORS

--- service-one/src/main/java/com/br/amqphw/ServiceOneApplication.java
+++ service-one/src/main/java/com/br/amqphw/ServiceOneApplication.java
@@ -2,11 +2,26 @@ package com.br.amqphw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ServiceOneApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(ServiceOneApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 }
